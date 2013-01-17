582de452190953ee1bd418df62a8e1c5a8c1c1dc
hiroolie <hiroolie@gmail.com>
2013-01-18 07:09:10 +0900

    Update scripts/VBA/makeAutoinst.vb
    Add comments

--- scripts/VBA/makeAutoinst.vb
+++ scripts/VBA/makeAutoinst.vb
@@ -1,40 +1,40 @@
 Sub makeAutoinst()
-    Dim xmlDoc As DOMDocument 'xml�f�[�^�p�ϐ�
-    Dim FileValue As Boolean '�ǂݍ��ݏ��ԗp
+    Dim xmlDoc As DOMDocument 'xmlデータ用変数
+    Dim FileValue As Boolean '読み込み状態用
     Dim SelNode As IXMLDOMNodeList
     Dim node As IXMLDOMNode
     Dim XmlPath As String
     Dim OutName As String
-    Dim w As Long, h As Long '�J�E���g�ϐ�
-    Dim SetVal As Variant, NodeVal As Variant '�Z���������ǂݍ��ݗp�ϐ�
+    Dim w As Long, h As Long 'カウント変数
+    Dim SetVal As Variant, NodeVal As Variant 'セル文字列読み込み用変数
     Dim OrgXML As String
     
-    OrgXML = Cells(1, 2) '��XML�t�@�C�������擾
+    OrgXML = Cells(1, 2) '元XMLファイル名を取得
     
-    XmlPath = ThisWorkbook.Path & "\" & OrgXML '�Z��B2�Ŏw�肵��xml���ǂݍ��݂܂�
+    XmlPath = ThisWorkbook.Path & "\" & OrgXML 'セルB2で指定したxmlを読み込みます
         
     Set xmlDoc = New DOMDocument
    
     xmlDoc.async = False
 
         For h = 4 To Cells(Rows.Count, 2).End(xlUp).Row
-            OutName = Cells(h, 3) '�o�̓t�@�C�������擾
+            OutName = Cells(h, 3) '出力ファイル名を取得
             
-            'XML�Ƃ��Ĉ����Ȃ��Ȃ镶�������폜
+            'XMLとして扱えなくなる文字列を削除
             Call ReplaceFromFile(XmlPath, ThisWorkbook.Path & "\tmp_" & OutName & ".xml", "<!DOCTYPE profile>")
                 
             If xmlDoc.Load(ThisWorkbook.Path & "\tmp_" & OutName & ".xml") Then
     
                 For w = 4 To Cells(4, Columns.Count).End(xlToLeft).Column
-                    NodeVal = Cells(3, w) 'XML�̃m�[�h���w�肷�镶����
-                    SetVal = Cells(h, w) 'NodeVal�ɑ��������l
+                    NodeVal = Cells(3, w) 'XMLのノードを指定する文字列
+                    SetVal = Cells(h, w) 'NodeValに代入する値
         
                     If SetVal <> True Then
-                        Set SelNode = xmlDoc.SelectNodes(NodeVal) '�Y�������m�[�h���z���Ɋi�[����
+                        Set SelNode = xmlDoc.SelectNodes(NodeVal) '該当するノードを配列に格納する
                         
-                        '�Ώۃ^�O�Ǎ�
-                        If SelNode.Length < 1 Then  '�L�����f
-                            '�������^�O�쐬
+                        '対象タグ読込
+                        If SelNode.Length < 1 Then  '有無判断
+                            '無い→タグ作成
                             Dim SelNode1 As IXMLDOMElement '
                             Dim NodeBefore As Variant
         
@@ -46,27 +46,18 @@ Sub makeAutoinst()
                         'MsgBox SelNode.Length
                         'MsgBox SelNode(0).Text
                         
-                        '���݁��f�[�^�ݒ�
-                        SelNode(0).Text = SetVal '�z�����ڂ�Setval��������
+                        '存在→データ設定
+                        SelNode(0).Text = SetVal '配列一つ目にSetvalを入れる
                     
                     End If
     
                 Next w
             
-                xmlDoc.Save (ThisWorkbook.Path & "\" & OutName & ".xml") '�����ĕۑ�
+                xmlDoc.Save (ThisWorkbook.Path & "\" & OutName & ".xml") 'そして保存
                 Kill (ThisWorkbook.Path & "\tmp_" & OutName & ".xml")
                 
             End If
         
      Next h
 
-End Sub
-Sub IP_DEC2HEX()
-    Dim strHex
-    Dim i As Long, tmp As Variant
-
-    For i = 3 To Cells(Rows.Count, 2).End(xlUp).Row
-        tmp = Split(Cells(i, 2), ".")
-        Cells(i, 3) = Right("0" & Hex(tmp(0)), 2) & Right("0" & Hex(tmp(1)), 2) & Right("0" & Hex(tmp(2)), 2) & Right("0" & Hex(tmp(3)), 2)
-        Next i
 End Sub
